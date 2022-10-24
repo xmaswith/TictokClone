53e92febcc6ae9a036e87841d6d03d0bfbecff47
john462 <xmaswith@gmail.com>
2022-10-24 12:29:49 +0800

    Initial commit

---
+++ app/src/main/java/com/pri/tictokclone/MainActivity.java
@@ -0,0 +1,29 @@
+package com.pri.tictokclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // status bar hide
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
