ea3a430a4d4f1a4bd21235118e07f4fe134a05bd
john462 <xmaswith@gmail.com>
2022-10-26 16:32:19 +0800

    10/26 commit

--- app/src/main/java/com/pri/tictokclone/HomeActivity.java
+++ app/src/main/java/com/pri/tictokclone/HomeActivity.java
@@ -2,13 +2,32 @@ package com.pri.tictokclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private CircleImageView civ_album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        civ_album = (CircleImageView) findViewById(R.id.civ_album);
+        Glide.with(this).load(R.mipmap.ic_album).into(civ_album);
+
+//        getWindow().getAttributes().flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
     }
+
 }
